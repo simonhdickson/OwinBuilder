4f0c960d3148abd48100c9c4d4912d8d2d7fbf6d
Simon Dickson <simonhdickson@gmail.com>
2013-12-05 21:18:40 +0000

    Intial commit

---
+++ src/OwinBuilder.Console/Framework.fs
@@ -0,0 +1,101 @@
+﻿namespace Skynerd
+open System
+open System.Collections.Generic
+open System.IO
+open System.Text
+open System.Threading.Tasks
+
+open FSharpx.Linq.QuotationEvaluation
+open Microsoft.FSharp.Quotations             
+open Microsoft.FSharp.Quotations.Patterns 
+open Microsoft.FSharp.Quotations.DerivedPatterns 
+open Newtonsoft.Json       
+open Owin
+
+module Server =
+    type ContentType =
+        | PlainText
+    
+    let requestTarget (enviroment:IDictionary<string,obj>) =
+        (enviroment.["owin.RequestMethod"] :?> string, enviroment.["owin.RequestPath"] :?> string)
+
+    let queryString (enviroment:IDictionary<string,obj>) =
+        let parameters =
+            enviroment.["owin.RequestQueryString"] :?> string
+            |> fun queryString -> queryString.Split '&'
+            |> Array.toList
+        match parameters with
+        | "" :: _-> Seq.empty
+        | _ -> parameters |> Seq.map (fun x -> ((x.Split '=').[0], (x.Split '=').[1]))
+
+    let headers (enviroment:IDictionary<string,obj>) =
+        enviroment.["owin.ResponseHeaders"] :?> IDictionary<string, string[]>
+
+    let requestBody (enviroment:IDictionary<string,obj>)=
+        (new StreamReader (enviroment.["owin.RequestBody"] :?> Stream)).ReadToEnd()
+
+    let responseBody (enviroment:IDictionary<string,obj>) =
+        enviroment.["owin.ResponseBody"] :?> Stream
+
+    let addResponseHeader (contentLength:int) contentType enviroment =
+        let responseHeaders = headers enviroment
+        responseHeaders.Add("Content-Length", [|contentLength.ToString()|])
+        match contentType with
+        | PlainText -> responseHeaders.Add("Content-Type", [|"text/plain"|])
+
+    let nextTask enviroment next =
+        Task.Run (fun () -> next enviroment)
+
+    let respondWith (response:string) enviroment =
+        let responseBytes = ASCIIEncoding.UTF8.GetBytes response
+        let responseStream = responseBody enviroment
+        addResponseHeader responseBytes.Length PlainText enviroment
+        responseStream.AsyncWrite(responseBytes, 0, responseBytes.Length) |> Async.StartAsTask :> Task
+
+    let rec parseParameters expression queryParameters =
+        seq {
+            match expression with
+                | Lambda (x, expr) when typeof<unit> = x.Type -> ()
+                | Lambda (x, expr) ->
+                    let value =
+                        queryParameters
+                        |> Seq.where (fun (i,_) -> x.Name = i)
+                        |> Seq.map (fun (x,i) -> i)
+                        |> Seq.head
+                    yield (value, x.Type)
+                    yield! parseParameters expr queryParameters  
+                | _ -> ()
+        }
+
+    let derserialize (value, valueType:Type) =
+        match valueType with
+        | t when typeof<string> = t -> value :> obj
+        | _ -> JsonConvert.DeserializeObject(value, valueType)
+
+    let invokeFunction (instance:Expr) parameters =
+        let instance = instance.EvalUntyped()
+        match Array.length parameters with
+        | 0 -> instance.GetType().GetMethods().[0].Invoke(instance, [|()|])
+        | _ -> instance.GetType().GetMethods().[0].Invoke(instance, parameters)
+
+    let (|>) a f = f a
+
+    let useRequestProcessor verb url (processor:Expr<'a->'b>) next enviroment =
+        let target = requestTarget enviroment
+        match target with
+        | (verb', url') when verb = verb' && url = url' ->
+            let response =
+                queryString enviroment
+                |> parseParameters processor
+                |> Seq.map derserialize
+                |> Seq.toArray
+                |> invokeFunction processor
+            respondWith (response.ToString()) enviroment
+        | _ ->
+            nextTask enviroment next
+
+    let Func2 (x:Func<_,_>) y = x.Invoke(y)
+
+    type IAppBuilder with
+        member x.UseRequestProcessor(verb:string, url:string, processor) =
+            x.Use(fun next -> useRequestProcessor verb url processor (Func2 next))
---
+++ src/OwinBuilder.Console/Program.fs
@@ -0,0 +1,60 @@
+﻿open System         
+open Owin
+open Microsoft.Owin
+open Microsoft.Owin.Hosting
+open Microsoft.FSharp.Quotations
+open FSharpx
+
+open Skynerd.Server
+
+module Owin =
+    open FSharpx.State
+    type OwinBuilder() =
+        member this.Return(a) : State<'T,'State> = fun s -> (a,s)
+        member this.ReturnFrom(m:State<'T,'State>) = m
+        member this.Bind(m:State<'T,'State>, k:'T -> State<'U,'State>) : State<'U,'State> = bind k m
+        member this.Zero() = this.Return ()
+        member this.Combine(r1, r2) = this.Bind(r1, fun () -> r2)
+        member this.TryWith(m:State<'T,'State>, h:exn -> State<'T,'State>) : State<'T,'State> =
+            fun env -> try m env
+                       with e -> (h e) env
+        member this.TryFinally(m:State<'T,'State>, compensation) : State<'T,'State> =
+            fun env -> try m env
+                       finally compensation()
+        member this.Using(res:#IDisposable, body) =
+            this.TryFinally(body res, (fun () -> match res with null -> () | disp -> disp.Dispose()))
+        member this.Delay(f) = this.Bind(this.Return (), f)
+        member this.While(guard, m) =
+            if not(guard()) then this.Zero() else
+                this.Bind(m, (fun () -> this.While(guard, m)))
+        member this.For(sequence:seq<_>, body) =
+            this.Using(sequence.GetEnumerator(),
+                (fun enum -> this.While(enum.MoveNext, this.Delay(fun () -> body enum.Current))))
+        [<CustomOperation("get", MaintainsVariableSpaceUsingBind=true)>]
+        member this.Get(m, url, processor) =
+            this.Bind(m, fun a ->
+                this.Bind(getState, fun (app:IAppBuilder) ->
+                    putState <| app.UseRequestProcessor("GET", url, processor)))
+    let owin = new OwinBuilder()
+
+[<ReflectedDefinition>]
+let cheese () =
+    "Hello World!"
+
+open Owin
+let pipeline = 
+    owin {   
+        get "/" <@ fun () -> "Hello World!" @> 
+        get "/talk" <@ fun name -> sprintf "Hello %s!" name @>
+        get "/cheese" <@ cheese @>
+    }
+
+let startServer (app:IAppBuilder) =
+    let result = State.exec pipeline app
+    ()
+
+[<EntryPoint>]
+let main argv =
+    WebApp.Start("http://localhost:5000", startServer) |> ignore
+    Console.ReadLine() |> ignore
+    0
